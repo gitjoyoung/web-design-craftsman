ab8832f4ed2a5f4794b6ee9b41cc5cc82619a12c
joyoung <githuborigin@gmail.com>
2024-09-06 14:54:39 +0900

    feat : 슬라이더 구현

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-$(function () {});
+$(function () {
+  var t = 0;
+  function slide() {
+    if (t < 2) {
+      t++;
+    } else {
+      t = 0;
+    }
+    $(".slide ul").animate({ top: t * 300 * -1 + "px" }, 1000);
+  }
+  setInterval(slide, 2000);
+});
