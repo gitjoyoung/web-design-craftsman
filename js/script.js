a1701d292bf21b805d0c8cd6b46e2c570d83780c
joyoung <githuborigin@gmail.com>
2024-09-10 12:42:00 +0900

    feat  : 팝업 , 탭 메뉴 자바스크립트 구현

--- js/script.js
+++ js/script.js
@@ -1,4 +1,35 @@
 $(function () {
+  // 탭 메뉴
+  $(".tabmenu li").click(function () {
+    $(".tabmenu li").removeClass("on");
+    $(this).addClass("on");
+    var i = $(this).index();
+    console.log(i);
+    $(".tabcon").hide();
+    $(".tabcon").eq(i).show();
+    return false;
+  });
+  // 팝업 열기
+  $(".pp").click(function () {
+    $(".popup").show();
+    $(".popup").show();
+    return false;
+  });
+  // 팝업 닫기
+  $(".popup button").click(function () {
+    $(".popup").hide();
+  });
+
+  // 메뉴 마우스 오버
+  $("nav>ul>li").mouseenter(function () {
+    $(".sub").stop().slideDown();
+  });
+  // 메뉴 마우스 아웃
+  $("nav>ul>li").mouseleave(function () {
+    $(".sub").stop().slideUp();
+  });
+
+  // 배너 슬라이드
   var t = 0;
   function slide() {
     if (t < 2) {
@@ -8,5 +39,6 @@ $(function () {
     }
     $(".slide ul").animate({ top: t * 300 * -1 + "px" }, 1000);
   }
+  // 2초마다 slide 함수 실행
   setInterval(slide, 2000);
 });
